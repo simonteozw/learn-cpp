92bfa1084e90d0dbc96f06fee85c9635948ba337
simonteozw <simonteozw@gmail.com>
2023-06-09 13:20:56 +0800

    cpp ex1

---
+++ basic/times2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main() {
+    int num{};
+
+    std::cout << "Enter an integer: ";
+
+    std::cin >> num;
+
+    std::cout << "Multiply by 2: " << num * 2 << "\n";
+    return 0;
+}
